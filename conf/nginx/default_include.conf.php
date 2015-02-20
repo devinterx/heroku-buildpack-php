a2203c7ff0b03c5b65485b7b3a65c386d024e3c4
devinterx <devinterx@gmail.com>
2015-02-20 17:21:06 +0200

    Fix Yii2 advanced template

--- conf/nginx/default_include.conf.php
+++ conf/nginx/default_include.conf.php
@@ -1,13 +1,24 @@
-location /acp {
-	try_files $uri $uri/ /acp.php?$args;
+location / {
+    index index.html index.php;
+	try_files $uri $uri/ /index.php?$args;
 }
 
-location / {
-    index  index.php index.html index.htm;
-	try_files $uri $uri/ /index.php?$args;
+location ~ /acp/(.*) {
+	index acp.php;
+	try_files /$uri /$uri/ /acp.php?$args;
+}
+
+location /assets {
+	alias D:/www/unibit_website/web/assets;
 }
 
 # for people with app root as doc root, restrict access to a few things
 location ~ ^/(composer\.|Procfile$|<?=getenv('COMPOSER_VENDOR_DIR')?>/|<?=getenv('COMPOSER_BIN_DIR')?>/) {
     deny all;
 }
+
+location ~ /\.(ht|htaccess|svn|git) {
+	deny all;
+}
+		
+		
