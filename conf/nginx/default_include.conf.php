360be480d66a1d86e39843f6a4dcfe137dfda497
devinterx <devinterx@gmail.com>
2015-02-20 18:45:17 +0200

    delete alias

--- conf/nginx/default_include.conf.php
+++ conf/nginx/default_include.conf.php
@@ -8,10 +8,6 @@ location ~ /acp/(.*) {
 	try_files /$uri /$uri/ /acp.php?$args;
 }
 
-location /assets {
-	alias D:/www/unibit_website/web/assets;
-}
-
 # for people with app root as doc root, restrict access to a few things
 location ~ ^/(composer\.|Procfile$|<?=getenv('COMPOSER_VENDOR_DIR')?>/|<?=getenv('COMPOSER_BIN_DIR')?>/) {
     deny all;
